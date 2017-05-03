9754acde7b9ea023b01c140079f0ea42c63262ca
zixian92 <zixian1992@hotmail.com>
2017-05-04 00:23:59 +0800

    Adds gl initialization calls

--- gl/utils.js
+++ gl/utils.js
@@ -10,6 +10,12 @@ export function initGL (userCanvas) {
     if (!canvas) {  // Global canvas and gl not initialized, initialize now
       canvas = document.createElement('canvas')
       gl = canvas.getContext('webgl') || canvas.getContext('experimental-webgl')
+      if (gl) { // Set up the GL object
+        gl.clearColor(0, 0, 0, 0)
+        gl.enable(gl.DEPTH_TEST)
+        gl.depthFunc(gl.LEQUAL)
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+      }
     }
     if (!gl) console.log('Unable to get WebGL context.')
     return gl
@@ -18,6 +24,12 @@ export function initGL (userCanvas) {
   // Attempt to get WebGL context for specified canvas so we can do GL and SL stuffs
   let finalGl = userCanvas.getContext('webgl') || userCanvas.getContext('experimental-webgl')
   if (!finalGl) console.log('Unable to get WebGL context.')
+
+  // Set up the GL object
+  finalGl.clearColor(0, 0, 0, 0)
+  finalGl.enable(finalGl.DEPTH_TEST)
+  finalGl.depthFunc(finalGl.LEQUAL)
+  finalGl.clear(finalGl.COLOR_BUFFER_BIT | finalGl.DEPTH_BUFFER_BIT)
 
   return finalGl
 }
