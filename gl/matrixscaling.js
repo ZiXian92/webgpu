9fc3902d9d12f07024429d92a91d03eec0f7f388
zixian92 <zixian1992@hotmail.com>
2017-05-04 01:42:19 +0800

    Implements the matrix scaling fragment shader

--- gl/matrixscaling.js
+++ gl/matrixscaling.js
@@ -4,13 +4,49 @@
 
 import { initGL } from './utils.js'
 
+const vertexPositions = [
+  0, 0, 0,
+  1, 0, 0,
+  1, 1, 0,
+  0, 1, 0
+]
+
 const vertexShaderSrc =
 `
+#ifdef GL_ES
+precision highp float;
+#endif
+attribute vec3 aVertexCoord;
+attribute vec2 aTextureCoord;
+varying vec2 vTextureCoord;
+
+void main() {
+  gl_Position = vec4(aVertexCoord, 1);
+  vTextureCoord = aTextureCoord;
+}
 `
 
 const fragmentShaderSrc =
 `
-
+#ifdef GL_ES
+precision highp float;
+#endif
+varying vec2 vTextureCoord;
+uniform sampler2D mtx;
+uniform float scaleFactor;
+uniform int numRows;
+uniform int numCols;
+float unpack(vec3 rgb) {
+  rgb*=vec3(255, 255, 255)
+  return round(rgb.x)*65536+round(rgb.y)*256+round(rgb.z);
+}
+vec3 pack(float val) {
+  return vec3(mod(val/65536, 256), mod(val/256, 256), mod(val, 256))/vec3(255, 255, 255);
+}
+void main() {
+  float newValue = scaleFactor*unpack(texture2D(mtx, vTextureCoord).xyz);
+  gl_FragColor = vec4(pack(newValue), 1);
+}
 `
 
 let gl = initGL()
@@ -26,6 +62,8 @@ vShader = gl.getShaderParameter(vShader, gl.COMPILE_STATUS) ? vShader : null
 fShader = gl.getShaderParameter(fShader, gl.COMPILE_STATUS) ? fShader : null
 
 let program = vShader && fShader ? gl.createProgram() : null
+let cubeVertexPositionBuffer
+let cubeTextureCoordBuffer
 if (program) {
   gl.attachShader(program, vShader)
   gl.attachShader(program, fShader)
