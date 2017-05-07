391198ba80432e80100cc96ab72d45fe47c82853
zixian92 <zixian1992@hotmail.com>
2017-05-07 14:13:34 +0800

    Implements matrix multiplication on GPU

--- components/MatrixMultiplier.jsx
+++ components/MatrixMultiplier.jsx
@@ -5,37 +5,57 @@
 
 import React from 'react'
 
+import matrixMultiply from '../gl/matrixmultiplication.js'
+
 import Matrix from './Matrix.jsx'
 
 export default class MatrixMultiplier extends React.Component {
   constructor () {
     super()
+    this.multiplyMatrices = this.multiplyMatrices.bind(this)
     this.N1 = 4
     this.M1 = this.N2 = 5
     this.M2 = 6
     let mtx1 = this.initMatrix(this.N1, this.M1)
     let mtx2 = this.initMatrix(this.N2, this.M2)
+    let outMtx = this.initMatrix(this.N1, this.M2, 0)
     this.state = {
-      mtx1, mtx2
+      mtx1, mtx2, outMtx
     }
   }
 
-  initMatrix (nRows, nCols) {
+  initMatrix (nRows, nCols, fillValue = null) {
     let mtx = []
     for (let r = 0; r < nRows; r++) {
       let row = []
-      for (let c = 0; c < nCols; c++) row.push(Math.round(Math.random() * 100000) / 100)
+      for (let c = 0; c < nCols; c++) row.push(fillValue === null ? Math.round(Math.random() * 100000) / 100.0 : fillValue)
       mtx.push(row)
     }
     return nRows < 2 ? mtx[0] : mtx
   }
 
+  multiplyMatrices (evt) {
+    let outMtx = matrixMultiply(this.state.mtx1, this.state.mtx2, this.M1, this.N1, this.M2, this.N2, 1)
+    this.setState({
+      outMtx
+    })
+  }
+
   render () {
     return (
       <div className="container-fluid">
+        <h2>Matrix Multiplication</h2>
+        <h3>Inputs</h3>
         <div className="row">
-          <Matrix className="col-4 col-offset-1" data={this.state.mtx1} rows={this.N1} cols={this.M1} />
-          <Matrix className="col-4 col-offset-7" data={this.state.mtx2} rows={this.N2} cols={this.M2} />
+          <Matrix className="col-sm-4 col-sm-offset-1" data={this.state.mtx1} rows={this.N1} cols={this.M1} />
+          <Matrix className="col-sm-4 col-sm-offset-1" data={this.state.mtx2} rows={this.N2} cols={this.M2} />
+        </div>
+        <h3>Output</h3>
+        <div className="row">
+          <Matrix className="col-sm-10 col-sm-offset-1" data={this.state.outMtx} rows={this.N1} cols={this.M2} />
+        </div>
+        <div className="text-center">
+          <button className="btn btn-primary" onClick={this.multiplyMatrices}>Compute</button>
         </div>
       </div>
     )
---
+++ gl/matrixmultiplication.js
@@ -0,0 +1,62 @@
+/**
+ * gl/matrixmultiplication.js
+ */
+
+import gpuutils from './utils.js'
+
+const fragmentShaderSrc = `
+${gpuutils.headerSrc}
+uniform sampler2D mtx1;
+uniform sampler2D mtx2;
+uniform int width1;
+${gpuutils.packValueSrc}
+${gpuutils.unpackValueSrc}
+void main() {
+  float increment = 1.0 / float(width1);
+  float startCoord =  increment / 2.0;
+  vec2 mtx1Coord = vec2(startCoord, vTextureCoord.t);
+  vec2 mtx2Coord = vec2(vTextureCoord.s, startCoord);
+  float ans = unpackValue(texture2D(mtx1, mtx1Coord)) * unpackValue(texture2D(mtx2, mtx2Coord));
+  for(int k=1; k<1024; k++) {
+    if (k >= width1) break;
+    mtx1Coord.s += increment;
+    mtx2Coord.t += increment;
+    ans += unpackValue(texture2D(mtx1, mtx1Coord)) * unpackValue(texture2D(mtx2, mtx2Coord));
+  }
+  gl_FragColor = packValue(ans);
+}
+`
+
+let program = gpuutils.makeKernel(fragmentShaderSrc, [
+  'mtx1', 'mtx2', 'width1'
+])
+
+export default function matrixMultiply (mtx1, mtx2, w1, h1, w2, h2, useGPU = 0) {
+  if (!useGPU || !program) {
+    if (!program) console.log('WebGL not supported. Falling back to CPU')
+    console.log('Matrix multiplication using CPU')
+
+    // Initialize result matrix
+    let mtx = []
+    for (let r = 0; r < h1; r++) {
+      let row = []
+      for (let c = 0; c < w2; c++) row.push(0)
+      mtx.push(row)
+    }
+
+    // Compute each element in result matrix
+    for (let i = 0; i < h1; i++)
+      for (let j = 0; j < w2; j++)
+        for (let k = 0; k < w1; k++)
+          mtx[i][j] += mtx1[i][k] * mtx2[k][j]
+    return mtx
+  }
+
+  return program([mtx1, mtx2], [{
+    name: 'mtx1', width: w1, height: h1
+  }, {
+    name: 'mtx2', width: w2, height: h2
+  }], {
+    width1: { type: 'int', value: w1 }
+  }, w2, h1)
+}
